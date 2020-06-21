038a81cac22e093f6d66466cf76652d3b3547b94
Marc Zyngier <maz@kernel.org>
2020-06-21 14:43:15 +0100

    irqchip/gic: Atomically update affinity
    commit 005c34ae4b44f085120d7f371121ec7ded677761 upstream.
    
    The GIC driver uses a RMW sequence to update the affinity, and
    relies on the gic_lock_irqsave/gic_unlock_irqrestore sequences
    to update it atomically.
    
    But these sequences only expand into anything meaningful if
    the BL_SWITCHER option is selected, which almost never happens.
    
    It also turns out that using a RMW and locks is just as silly,
    as the GIC distributor supports byte accesses for the GICD_TARGETRn
    registers, which when used make the update atomic by definition.
    
    Drop the terminally broken code and replace it by a byte write.
    
    Fixes: 04c8b0f82c7d ("irqchip/gic: Make locking a BL_SWITCHER only feature")
    Cc: stable@vger.kernel.org
    Signed-off-by: Marc Zyngier <maz@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    
    Signed-off-by: grm34 <jerem.pardo@tutanota.com>

--- drivers/irqchip/irq-gic.c
+++ drivers/irqchip/irq-gic.c
@@ -324,44 +324,18 @@ static int gic_irq_set_vcpu_affinity(struct irq_data *d, void *vcpu)
 static int gic_set_affinity(struct irq_data *d, const struct cpumask *mask_val,
 			    bool force)
 {
-	void __iomem *reg = gic_dist_base(d) + GIC_DIST_TARGET + (gic_irq(d) & ~3);
-	unsigned int cpu, shift = (gic_irq(d) % 4) * 8;
-	u32 val, mask, bit;
-	unsigned long flags;
+	void __iomem *reg = gic_dist_base(d) + GIC_DIST_TARGET + gic_irq(d);
+	unsigned int cpu;
 
-	gic_lock_irqsave(flags);
-	if (unlikely(d->common->state_use_accessors & IRQD_GIC_MULTI_TARGET)) {
-		struct cpumask temp_mask;
+	if (!force)
+		cpu = cpumask_any_and(mask_val, cpu_online_mask);
+	else
+		cpu = cpumask_first(mask_val);
 
-		bit = 0;
-		if (!cpumask_and(&temp_mask, mask_val, cpu_online_mask))
-			goto err_out;
-#ifndef CONFIG_SCHED_HMP
-		if (!cpumask_and(&temp_mask, &temp_mask, cpu_coregroup_mask(0)))
-			goto err_out;
-#endif
-		for_each_cpu(cpu, &temp_mask) {
-			if (cpu >= NR_GIC_CPU_IF || cpu >= nr_cpu_ids)
-				goto err_out;
-			bit |= gic_cpu_map[cpu];
-		}
-		bit <<= shift;
-	} else {
-		if (!force)
-			cpu = cpumask_any_and(mask_val, cpu_online_mask);
-		else
-			cpu = cpumask_first(mask_val);
+	if (cpu >= NR_GIC_CPU_IF || cpu >= nr_cpu_ids)
+		return -EINVAL;
 
-		if (cpu >= NR_GIC_CPU_IF || cpu >= nr_cpu_ids)
-				goto err_out;
-
-		bit = gic_cpu_map[cpu] << shift;
-	}
-	mask = 0xff << shift;
-	val = readl_relaxed_no_log(reg) & ~mask;
-	writel_relaxed_no_log(val | bit, reg);
-	gic_unlock_irqrestore(flags);
-
+	writeb_relaxed(gic_cpu_map[cpu], reg);
 	irq_data_update_effective_affinity(d, cpumask_of(cpu));
 
 	return IRQ_SET_MASK_OK_DONE;
