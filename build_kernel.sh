e1513f74aa2b87c77837a7930564a1b4d695fba6
Samuel Pascua <sgpascua@ngcp.ph>
2021-12-18 01:40:56 +0000

    arm64: enable VDSO_COMPAT support
    Signed-off-by: Samuel Pascua <sgpascua@ngcp.ph>

--- build_kernel.sh
+++ build_kernel.sh
@@ -4,6 +4,7 @@ export ARCH=arm64
 mkdir out
 
 BUILD_CROSS_COMPILE=/home/pascua14/gcc-7.4.1/bin/aarch64-linux-gnu-
+BUILD_CROSS_COMPILE_COMPAT=/home/pascua14/gcc-4.9/bin/arm-linux-androideabi-
 KERNEL_LLVM_BIN=/home/pascua14/clang/bin/clang
 CLANG_TRIPLE=aarch64-linux-gnu-
 KERNEL_MAKE_ENV="DTC_EXT=$(pwd)/tools/dtc CONFIG_BUILD_ARM64_DT_OVERLAY=y"
@@ -14,7 +15,10 @@ echo "(1) 4G Variant"
 echo "(2) 5G Variant"
 read -p "Selected variant: " variant
 
-make -j8 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE r8q_defconfig
+make -j8 -C $(pwd) O=$(pwd)/out \
+$KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE \
+CROSS_COMPILE_COMPAT=$BUILD_CROSS_COMPILE_COMPAT \
+CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE r8q_defconfig
 
 if [ $variant == "1" ]; then
 	echo "
@@ -44,11 +48,17 @@ CONFIG_FIVE
 # CONFIG_FIVE is not set
 " >> out/.config
 
-make -j8 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE oldconfig
+make -j8 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV \
+ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE \
+CROSS_COMPILE_COMPAT=$BUILD_CROSS_COMPILE_COMPAT \
+CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE oldconfig
 
 fi
 
-make -j8 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE
+make -j8 -C $(pwd) O=$(pwd)/out \
+$KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE \
+CROSS_COMPILE_COMPAT=$BUILD_CROSS_COMPILE_COMPAT \
+CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE
 
 cat out/arch/arm64/boot/dts/vendor/qcom/*.dtb > out/dtb.img
 
--- regen.sh
+++ regen.sh
@@ -5,6 +5,7 @@ mkdir out
 
 KERNEL_LLVM_BIN=/home/pascua14/clang/bin
 KERNEL_MAKE_ENV="DTC_EXT=$(pwd)/tools/dtc CONFIG_BUILD_ARM64_DT_OVERLAY=y"
+BUILD_CROSS_COMPILE_COMPAT=/home/pascua14/gcc-4.9/bin/arm-linux-androideabi-
 
 scripts/configcleaner "
 CONFIG_SAMSUNG_NFC
@@ -21,6 +22,7 @@ CONFIG_NFC_FEATURE_SN100U=y
 " >> out/.config
 
 make -j8 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 \
+	CROSS_COMPILE_COMPAT=$BUILD_CROSS_COMPILE_COMPAT \
 	CC=$KERNEL_LLVM_BIN/clang \
 	oldconfig
 
